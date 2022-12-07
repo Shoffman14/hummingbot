ad16c78260646a57b9962ea363d016538492c39d
Sh14 <60073370+Shoffman14@users.noreply.github.com>
2022-12-06 20:48:25 -0500

    Add files via upload

---
+++ scripts/TESTING_MA_Trigger_v8_ETHUSDT_Dec062022.py
@@ -0,0 +1,62 @@
+from decimal import Decimal
+from statistics import mean
+from collections import deque
+import logging
+
+
+
+# from hummingbot.client.hummingbot_application import HummingbotApplication
+from hummingbot.core.data_type.common import OrderType, PositionAction
+from hummingbot.core.event.events import BuyOrderCreatedEvent
+from hummingbot.core.rate_oracle.rate_oracle import RateOracle
+from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
+
+class MATriggers(ScriptStrategyBase):
+
+
+    markets = {
+        "bybit_perpetual": {"ETH-USDT"}
+    }
+    #: pingpong is a variable to allow alternating between buy & sell signals
+    pingpong = 0
+    de_fast_ma = deque([], maxlen=1200)
+    de_slow_ma = deque([], maxlen=12000)
+
+
+
+    def on_tick(self):
+        p = self.connectors["bybit_perpetual"].get_price("ETH-USDT", True)
+
+        #: with every tick, the new price of the trading pair_pair will be appended to the dque and MA will be calculated
+        self.de_fast_ma.append(p)
+        self.de_slow_ma.append(p)
+        fast_ma = mean(self.de_fast_ma)
+        slow_ma = mean(self.de_slow_ma)
+
+        #: logic for golden cross
+        if (fast_ma > slow_ma) & (self.pingpong == 0):
+            self.buy(
+                connector_name="bybit_perpetual",
+                trading_pair="ETH-USDT",
+                amount=Decimal(0.8),
+                order_type=OrderType.MARKET,
+                price=p,
+                position_action=PositionAction.OPEN
+            )
+            self.logger().info(f'{"0.8 ETH bought"}') #[12/6/22]: 0.8 is ~ $1,000USDT worth
+            self.pingpong = 1
+
+        #: logic for death cross
+        elif (slow_ma > fast_ma) & (self.pingpong == 1):
+            self.sell(
+                connector_name="bybit_perpetual",
+                trading_pair="ETH-USDT",
+                amount=Decimal(0.8),
+                order_type=OrderType.MARKET,
+                price=p,
+                position_action=PositionAction.OPEN
+            )
+            self.logger().info(f'{"0.8 DOGE sold"}') #[12/6/22]: 19666 is ~ $1,000USDT worth
+            self.pingpong = 0
+        else:
+            self.logger().info(f'{"wait for a signal to be generated"}')
